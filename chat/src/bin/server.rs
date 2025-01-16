2d6950f6d52b2866ad4742465c731542a7ed54db
sonhmai <14060682+sonhmai@users.noreply.github.com>
2025-01-16 11:13:49 +0700

    implement not sending msg to the sender client but not success yet

--- chat/src/bin/server.rs
+++ chat/src/bin/server.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
 use futures_util::sink::SinkExt;
 use futures_util::stream::StreamExt;
 use std::error::Error;
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast::{channel, Sender};
 use tokio_websockets::{Message, ServerBuilder, WebSocketStream};
@@ -13,6 +15,7 @@ async fn handle_connection(
     addr: SocketAddr,
     mut ws_stream: WebSocketStream<TcpStream>,
     bcast_tx: Sender<String>,
+    clients: Arc<Mutex<HashMap<SocketAddr, Sender<String>>>>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
 
     ws_stream
@@ -28,7 +31,12 @@ async fn handle_connection(
                     Some(Ok(msg)) => {
                         if let Some(text) = msg.as_text() {
                             println!("From client {addr:?} {text:?}");
-                            bcast_tx.send(text.into())?;
+                            let clients = clients.lock().unwrap();
+                            for (client_addr, client_tx) in clients.iter() {
+                                if *client_addr != addr {
+                                    client_tx.send(text.into()).unwrap(); // Send to all except sender
+                                }
+                            }
                         }
                     }
                     Some(Err(err)) => return Err(err.into()),
@@ -46,19 +54,22 @@ async fn handle_connection(
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let (bcast_tx, _) = channel(16);
-
+    // Arc instead of Rc as multiple tasks can mutate this in different threads.
+    // Rc is only for single-threaded env, Arc provides atomic ref count update.
+    let clients = Arc::new(Mutex::new(HashMap::new()));
     let listener = TcpListener::bind("127.0.0.1:2000").await?;
-    println!("listening on port 2000");
+    println!("server listening on port 2000");
 
     loop {
         let (socket, addr) = listener.accept().await?;
         println!("New connection from {addr:?}");
         let bcast_tx = bcast_tx.clone();
+        let clients = Arc::clone(&clients);
         tokio::spawn(async move {
+            clients.lock().unwrap().insert(addr, bcast_tx.clone());
             // Wrap the raw TCP stream into a websocket.
             let ws_stream = ServerBuilder::new().accept(socket).await?;
-
-            handle_connection(addr, ws_stream, bcast_tx).await
+            handle_connection(addr, ws_stream, bcast_tx, clients).await
         });
     }
 }
